665999726b13e61ce775528f0fea272197396a4b
Yuki Yoshitome <y.f.m.aquar.phy@gmail.com>
2023-05-28 13:50:42 +0900

    Initial commit

---
+++ fumotoppara_rsv.py
@@ -0,0 +1,63 @@
+import time
+from selenium.webdriver.common.by import By
+from mylib import ScrFunc
+
+weekday = '土'
+
+test_mode = False
+def main():
+    pre_result = [] #空きの変化検出用
+
+    while(True):
+            
+        #Init処理
+        driver = ScrFunc.GetDriver()
+
+        #空き曜日を検出してリストに格納
+        available_day = Getavailable(driver)
+        #LINEで空き日程を通知.
+        if ( pre_result != available_day):
+            ScrFunc.Notify_LINE(available_day)
+            pre_result = available_day
+
+        driver.quit()
+        time.sleep(300)#5分に1回スクレイピング
+
+def Getavailable(driver):
+    ret = []
+    offset = 0
+    date_xpath = []
+    for i in range(124):
+        date_xpath.append(f'//*[@id="__layout"]/section/section/main/form/div[2]/div[2]/table/thead/tr/th[{str(i+2)}]/p[1]')
+    #idを入れたらそのまま日付を返すリスト
+    for index in range(7):
+        chk_xpath = f'//*[@id="__layout"]/section/section/main/form/div[2]/div[2]/table/thead/tr/th[{str(index+2)}]/p[2]'
+        #1は例外. 2から曜日が始まるのでindex+2.
+        weekday_column = driver.find_element(By.XPATH, chk_xpath)
+        if (weekday_column.text == weekday):
+            offset = index + 2
+            break
+    if(test_mode):print(f'最初の{weekday_column.text}曜日は{offset-1}番目のセル')
+    
+    id = 0 #7の倍数を足して-1して引数に入れれば空き状況を返すインデックス.先頭から何番目か.
+    #最初の土曜キャンプ宿泊へのXPATH.これ以降は7ずつ足す.
+    for index in range(20):
+        id = offset + index*7 - 2
+        first_xpath=f'//*[@id="__layout"]/section/section/main/form/div[2]/div[2]/table/tbody/tr[2]/td[{id+1}]'
+        val = driver.find_element(By.XPATH,first_xpath).text
+        if(val != "ー"):break
+    for index in range(20):
+        temp_xpath = f'//*[@id="__layout"]/section/section/main/form/div[2]/div[2]/table/tbody/tr[2]/td[{id+1}]/p[1]'
+        val = driver.find_element(By.XPATH,temp_xpath).text
+        #val2 =(driver.find_element(By.XPATH,temp_xpath2)).text
+        if (test_mode):print(val)
+        if(val == "ー"):break
+        elif (val != '×'):
+            ret.append((driver.find_element(By.XPATH, date_xpath[id])).text)
+        id += 7
+       
+    return ret
+
+
+if __name__ == "__main__":
+    main()
---
+++ mylib/ScrFunc.py
@@ -0,0 +1,1 @@
+#ScrFunc.py private
