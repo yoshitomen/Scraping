d9be1a3bf3e500c90db0b07bc25ff4ec151350c8
yoshitomen <y.f.m.aquar.phy@gmail.com>
2023-07-15 12:46:35 +0900

    Continue actions in continuous vacant coat

--- src/tokyo_tenniscoat_act.py
+++ src/tokyo_tenniscoat_act.py
@@ -1,11 +1,14 @@
-import sys
+import time, sys
 from os.path import dirname, abspath
 sys.path.append(dirname(dirname(abspath(__file__))))
 from mylib.scraping.src import tokyo_tenniscoat_mac  as mac_ver
 
 def main():
-    mac_ver.tokyo_tenniscoat()
-
+    #initialize
+    result = mac_ver.tokyo_tenniscoat()
+    while(result):
+        time.sleep(300)
+        result = mac_ver.tokyo_tenniscoat(result)
 
 if __name__ == "__main__":
     main()
